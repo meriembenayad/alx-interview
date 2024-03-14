3c251acfd63b9c95755be455909da58add7dd74f
meriembenayad <meriem.benayad@gmail.com>
2024-03-14 21:17:18 +0000

    Lockbox

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,21 +7,26 @@ def canUnlockAll(boxes):
     Method that determines if all the boxes can be opened
     """
     # the first box is already opened.
-    opened_boxes = {0}
+    opened_boxes = [0]
     # add keys from the first box in a set of keys
     keys = set(boxes[0])
 
     # while there are keys can open boxes that still locked
-    while keys - opened_boxes:
-        # key that can open a box still locked
-        key = (keys - opened_boxes).pop()
-        # we add it to the set of opened_boxes
-        opened_boxes.add(key)
-        # we add the keys from the boxwe just opened to our set
-        keys.update(boxes[key])
+    while keys:
+        new_keys = set()
+        for key in keys:
+            # if key corresponds to a box and the box still locked
+            if key < len(boxes) and key not in opened_boxes:
+                # add the box to opened_boxes
+                opened_boxes.append(key)
+                # Add new keysfound in thebox to new_keys
+                new_keys.update(boxes[key])
+        # if no new keys found, break the loop
+        if not new_keys:
+            break
 
-    # Check if the opened_box is equal to number of boxe, return True
-    if len(opened_boxes) == len(boxes):
-        return True
-    else:
-        return False
+        # Update keys with newly found keys for next iteration
+        keys = new_keys
+
+    # if opened boxes is equal to number of boxes, all can be opened
+    return len(opened_boxes) == len(boxes)
